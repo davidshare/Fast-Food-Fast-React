ab7c5c868087ea029266836a41dbc54a5228c292
davidshare <davidessienshare@gmail.com>
2019-01-13 01:11:39 +0100

    chore(reduxSetup): add redux functionality to the app
    - install redux, react-redux, redux-thunk, axios
    - create a store directory
    - add files for the initial state and the store configuration
    - create a directory for reducers, and add an index file for the combineReducer
    - create a director for actions, add an index file and file for actionTypes
    - edit the App component to use the redux provider
    
    [Finishes #163152854]

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,27 @@
 import React, { Component, Fragment } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import LandingPage from './landingPage/landingPage';
 import '../styles/app.css'
 import Aux from './hoc/aux';
+import store from '../store/store';
 
 
 
 class App extends Component {
     render(){
-		return <Aux>
-			<BrowserRouter>
-				<Switch>
-					<Route exact path='/' component={LandingPage}/>
-				</Switch>
-			</BrowserRouter>
-        </Aux>
+		return (
+			<Provider store={store}>
+				<Aux>
+					<BrowserRouter>
+						<Switch>
+							<Route exact path='/' component={LandingPage}/>
+						</Switch>
+					</BrowserRouter>
+				</Aux>
+			</Provider>
+		)
     }
 }
 
