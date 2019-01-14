8e85b1849609266c27039daa82212a995d11fba3
David Itam Essien <davidessienshare@gmail.com>
2019-01-14 07:38:50 +0100

    #163152854 Add redux for state management (#5)
    * chore(reduxSetup): add redux functionality to the app
    
    - install redux, react-redux, redux-thunk, axios
    - create a store directory
    - add files for the initial state and the store configuration
    - create a directory for reducers, and add an index file for the combineReducer
    - create a director for actions, add an index file and file for actionTypes
    - edit the App component to use the redux provider
    
    [Finishes #163152854]

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import LandingPage from './landingPage/landingPage';
 import '../styles/app.css';
 import Aux from './hoc/aux';
+import store from '../store/store';
 
 /**
  * @descrption The App class component for rendering the app
@@ -15,13 +17,15 @@ class App extends Component {
 */
   render() {
     return (
-    <Aux>
-    <BrowserRouter>
-      <Switch>
-        <Route exact path='/' component={LandingPage}/>
-        </Switch>
-      </BrowserRouter>
-    </Aux>
+      <Provider store={store}>
+        <Aux>
+          <BrowserRouter>
+            <Switch>
+              <Route exact path='/' component={LandingPage}/>
+            </Switch>
+          </BrowserRouter>
+        </Aux>
+      </Provider>
     );
   }
 }
